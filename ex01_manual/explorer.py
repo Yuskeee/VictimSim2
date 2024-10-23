4379f21c1f0f5c4e447e92df568b7c9ab638f5e8
Yuskeee <rodrigo.yuske@gmail.com>
2024-10-23 10:37:41 -0300

    Added on-line DFS to explorer

--- ex01_manual/explorer.py
+++ ex01_manual/explorer.py
@@ -25,8 +25,8 @@ class Explorer(AbstAgent):
         # List of visited coordinates
         self.visited = []
 
-        # List of not yet visited coordinates
-        self.not_visited = []
+        # List of backtracked coordinates
+        self.backtracked = []
 
         # List of coordinates with obstacles
         self.obstacles = []
@@ -38,36 +38,57 @@ class Explorer(AbstAgent):
     # Neighbor Index to Position
     def neighbor_index_to_position(self, current_coordinates, index):
         if index == 0:
-            return (current_coordinates.x, current_coordinates.y - 1)
+            return (current_coordinates[0], current_coordinates[1] - 1)
         elif index == 1:
-            return (current_coordinates.x + 1, current_coordinates.y - 1)
+            return (current_coordinates[0] + 1, current_coordinates[1] - 1)
         elif index == 2:
-            return (current_coordinates.x + 1, current_coordinates.y)
+            return (current_coordinates[0] + 1, current_coordinates[1])
         elif index == 3:
-            return (current_coordinates.x + 1, current_coordinates.y + 1)
+            return (current_coordinates[0] + 1, current_coordinates[1] + 1)
         elif index == 4:
-            return (current_coordinates.x, current_coordinates.y + 1)
+            return (current_coordinates[0], current_coordinates[1] + 1)
         elif index == 5:
-            return (current_coordinates.x -1, current_coordinates.y + 1)
+            return (current_coordinates[0] -1, current_coordinates[1] + 1)
         elif index == 6:
-            return (current_coordinates.x - 1, current_coordinates.y)
+            return (current_coordinates[0] - 1, current_coordinates[1])
         elif index == 7:
-            return (current_coordinates.x - 1, current_coordinates.y - 1)
+            return (current_coordinates[0] - 1, current_coordinates[1] - 1)
 
-    # Exploration: BFS
+    # Exploration: DFS
     def explore(self):
         self.visited.append((self.x, self.y))  # Adds the current position to visited
+        self.backtracked.append((self.x, self.y))  # Adds the current position to backtracked
 
         neighbors = self.check_walls_and_lim()
+        valid_neighbors = []
 
-        for i in range(neighbors):
-            if neighbors[i] == VS.CLEAR:
-                self.not_visited.append(self.neighbor_index_to_position((self.x, self.y), i))
+        for i in range(len(neighbors)):
+            if neighbors[i] == VS.CLEAR: # Adds valid neighbors to local list
+                if self.neighbor_index_to_position((self.x, self.y), i) not in self.visited: # Checks if the neighbor is already visited
+                    valid_neighbors.append(self.neighbor_index_to_position((self.x, self.y), i))
             else:
                 # It does not differentiate between walls and end of map
                 self.obstacles.append(self.neighbor_index_to_position((self.x, self.y), i))
         
-        return self.not_visited.pop()
+        # Checks if should backtrack
+        if len(valid_neighbors) == 0:
+            if len(self.backtracked) <= 1:
+                print(f"{self.NAME} No more places to explore... invoking the rescuer")
+                self.resc.go_save_victims([],[])
+                return (self.x, self.y)
+            else:
+                # Backtracks to the last position
+                self.backtracked.pop()
+                next_position = self.backtracked.pop()
+                return next_position
+
+        # Pops the next position to explore (maybe with random choice)
+        next_position = valid_neighbors.pop(random.randint(0, len(valid_neighbors) - 1))
+        return next_position
+
+    def return_to_base(self):
+        # Returns to the base using A* algorithm
+        pass
 
     def deliberate(self) -> bool:
         """ The agent chooses the next action. The simulator calls this
@@ -85,9 +106,15 @@ class Explorer(AbstAgent):
         dx = 0
         dy = 0
 
-        self.explore()
+        (next_x, next_y) = self.explore()
 
-        print(f"{self.NAME} exploring startes")
+        # Update dx and dy based on choice made through explore()
+        dx = next_x - self.x
+        dy = next_y - self.y
+        self.x = next_x
+        self.y = next_y
+
+        print(f"{self.NAME} exploring starts")
         
         # Moves the body to another position
         result = self.walk(dx, dy)
